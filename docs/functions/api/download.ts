4c475a4690f33362323d5c783541bb84d60b53cc
genteure <genteure@gmail.com>
2022-04-25 21:38:37 +0800

    add functions

---
+++ docs/functions/api/download.ts
@@ -0,0 +1,26 @@
+import { generateSignedURL } from "../utils/signedUrl";
+
+export const onRequestGet: PagesFunction<{
+  IMAGES: KVNamespace;
+  DOWNLOAD_COUNTER: DurableObjectNamespace;
+}> = async (context) => {
+  const {
+    request, // same as existing Worker API
+    env, // same as existing Worker API
+    params, // if filename includes [id] or [[path]]
+    waitUntil, // same as ctx.waitUntil in existing Worker API
+    next, // used for middleware or to fetch assets
+    data, // arbitrary space for passing data between middlewares
+  } = context;
+
+  throw "not implemented";
+  return new Response(null, {
+    headers: {
+      Location: await generateSignedURL({
+        url: `${previewURLBase}/highres`,
+        imagesKey,
+      }),
+    },
+    status: 302,
+  });
+};
