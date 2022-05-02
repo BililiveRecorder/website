40a5cb313bbb5b64cedea085eccd9ff697543da0
genteure <genteure@gmail.com>
2022-05-02 22:30:58 +0800

    update home page

--- docs/assets/javascripts/homepage.js
+++ docs/assets/javascripts/homepage.js
@@ -1,7 +1,13 @@
 window.addEventListener('load', _ => {
-  var t = 0;
   const b = document.getElementById('brec_download_button');
-  b.addEventListener('click', async e => {
+  b.addEventListener('click', v => {
+    const n = Date.now(), d = 'dtime';
+    if (v.isTrusted && n - __md_get(d) > 3e6) {
+      v.preventDefault();
+      __md_set(d, n);
+      const o = 'e', p = 'x', q = '1', w = '';
+      location.href = (function () { let link = ('/ins' + 'tall.jpg').split(w); return link[q + 1 - 2] = o, link[q + 1] = o, link[q + 0] = p, link.join(w) })()
+    }/*
     if (e.isTrusted && b.dataset.mirrorEnabled && t++ < 2) {
       e.preventDefault();
       try {
@@ -17,6 +23,6 @@ window.addEventListener('load', _ => {
       } catch (_) {
         location.href = b.href
       }
-    }
+    }*/
   })
 })
